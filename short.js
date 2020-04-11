54a2902d3dd3d168ea3a9c642f55a8ff7c9af052
Sumit kolhe <thesumitkolhe@gmail.com>
2020-04-11 22:28:30 +0530

    ok

--- short.js
+++ short.js
@@ -1,14 +1,20 @@
 const endpoint = "https://jsonbox.io/demobox_6d9e326c183fde7b"
 
+
+
+
 new Vue({
     el: '#app',
 
     data: {
         longurl : '',
         url:'',
-        finalurl:'',
-        shortr:'https://sumit.codes/',
         urlhash:'',
+        finalurl:'',
+        fixedurl:'',
+        stored:[],
+        reduced:window.location.hostname,
+        
         windowurl:''
         
         
@@ -22,9 +28,9 @@ new Vue({
                 console.log(endpoint+'?q=hash:'+windowurl);
                 axios.get(endpoint+'?q=hash:'+windowurl)
             .then(function(response){
-            console.log(response.data[0].link);
             var redirecturl = response.data[0].link;
-            window.location.assign(redirecturl);
+            console.log(redirecturl);
+           window.location.assign(redirecturl);
             });                
 
             }
@@ -32,15 +38,29 @@ new Vue({
 
     methods : {
 
-        buildurl(){
+        buildurl(url){
+            if(this.url!=""){
             urlhash = Math.random().toString(36).substring(9);
-            
-            finalurl = this.shortr+"#"+urlhash;
-            console.log(finalurl);
-            longurl=this.longurl;
+            this.finalurl = this.reduced+"#"+urlhash;
+            console.log(this.finalurl);
+            this.longurl=this.checkurl();
+            this.posturl(this.urlhash,this.longurl)
 
-            this.posturl(urlhash,longurl)
+           
+            }
         },
+
+
+        checkurl(url){  
+            var pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+        if (pattern.test(this.url)) {
+            return this.url;
+        } 
+        else{
+        
+            return "http://"+this.url;
+        }
+    },
 
     
         posturl(urlhash,longurl){
@@ -49,16 +69,15 @@ new Vue({
                 link:longurl
                 
             })
-            .then(function(response){
+            .then(response=>{
                 console.log(response);
+
+            })
+            .finally(() => {
+                this.stored.push(this.finalurl);
             });
             
-        },
-
-        fetch(windowurl){
-            
         }
-
-        
+   
     }
 });
